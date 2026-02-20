69828cfc60cbc7735e93ffa76c05c0ff835edca4
Regina Steinberg <regina.steinberg@fau.de>
2026-02-20 10:04:17 +0100

    fix: vendor loading

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rrze/block-control',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ac0c58dcdad1cd752a8b5494fcce80c0eddcb94f',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rrze/block-control' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ac0c58dcdad1cd752a8b5494fcce80c0eddcb94f',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
